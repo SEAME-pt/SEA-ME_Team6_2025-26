9228ad682890b2f50aaac0e1707a62ece517e0be
souzitaaaa <diogo.nogueirasousa123@gmail.com>
2025-11-01 17:42:13 +0000

    feat: qtquick integration and cross compiler adaptation

---
+++ src/cluster/build/Desktop_Qt_6_9_3-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_HelloQt6Qml_FILE /home/dinoguei/Desktop/SEAME/SEA-ME_Team6_2025-26V4/src/cluster/build/Desktop_Qt_6_9_3-Debug/HelloQt6Qml)
+set(__QT_DEPLOY_TARGET_HelloQt6Qml_TYPE EXECUTABLE)
--- src/cluster/main.cpp
+++ src/cluster/main.cpp
@@ -1,15 +1,16 @@
-#include <QApplication>
-#include <QLabel>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+    QGuiApplication app(argc, argv);
 
-    QLabel label("SEA:ME");
-    label.setWindowTitle("Hello Qt6");
-    label.setAlignment(Qt::AlignCenter);
-
-    label.showFullScreen();
+    QQmlApplicationEngine engine;
+    const QUrl url(u"qrc:/main.qml"_qs);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
+                     &app, []() { QCoreApplication::exit(-1); },
+                     Qt::QueuedConnection);
+    engine.load(url);
 
     return app.exec();
 }
