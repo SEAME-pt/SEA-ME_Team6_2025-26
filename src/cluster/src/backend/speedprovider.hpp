1c91fd9afc4f1136029a902bcc63a4504ba2803e
souzitaaaa <diogo.nogueirasousa123@gmail.com>
2025-12-17 15:15:30 +0000

    feat(tests): First Coverage Report on QT Backend

--- src/cluster/src/backend/speedprovider.hpp
+++ src/cluster/src/backend/speedprovider.hpp
@@ -1,7 +1,20 @@
+/**
+ * @author souzitaaaa
+ * @email diogo.nogueirasousa123@gmail.com
+ * @create date 2025-12-16 14:40:18
+ * @modify date 2025-12-16 14:42:44
+ * @desc Class that contains speed functions and exposition to QML
+ */
+
+#ifndef speedprovider
+#define speedprovider
+
 #include <QObject>
 #include <QTimer>
+#include <QtMath>
 
-class SpeedProvider : public QObject {
+class SpeedProvider : public QObject
+{
     Q_OBJECT
     Q_PROPERTY(QString currSpeed READ currSpeed NOTIFY speedChanged)
 
@@ -10,15 +23,15 @@ public:
 
     QString currSpeed() const;
 
+public slots:
+    void setSpeed(double speed);
+
 signals:
     void speedChanged();
 
-private slots:
-    void updateSpeed();
-
 private:
     QString _currSpeed;
-    QTimer  _timer;
     double _speedValue = 0.0;
-    double _increment  = 0.5;
 };
+
+#endif /* speedprovider */
