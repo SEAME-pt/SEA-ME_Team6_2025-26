953854556d1af48cfad754c077cda1c53411a038
daviduartecf <daviduartecf@gmail.com>
2026-02-26 14:37:47 +0000

    feat - Added scenario_loader helpers for can simulator

---
+++ src/kuksa/kuksa_RPi5/src/can_simulator/inc/scenario_loader.hpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <fstream>
+#if __has_include(<filesystem>)
+  #include <filesystem>
+  namespace fs = std::filesystem;
+#else
+  #error "C++17 <filesystem> required"
+#endif
+
+#include "app_context.hpp"
+#include "helpers.hpp"
+
+std::vector<Scenario> load_scenarios_dir(const std::string& dir);
+Scenario load_scenario_file(const fs::path& p);
